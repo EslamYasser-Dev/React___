85af247bf7844ca340bc4d9d466f5d5453cb3811
eslamyasser <e.yasser997@gmail.com>
2023-05-28 00:11:28 +0300

    ClassProps

--- src/MyComps/ClassProps.jsx
+++ src/MyComps/ClassProps.jsx
@@ -1,19 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { Component } from 'react';
 
-class ClassProp extends Component {
-
-ClassProp
-
-
+class ClassProps extends Component {
   render() {
+    const { course1, course2, childElement } = this.props;
+    
     return (
-        <div>
-          
-        </div>
-    )
+      <div>
+        <h3>Courses:</h3>
+        <ul>
+          <li>{course1}</li>
+          <li>{course2}</li>
+        </ul>
+        <div>{childElement}</div>
+      </div>
+    );
   }
 }
 
-
-
-export default ClassProp;
+export default ClassProps;
